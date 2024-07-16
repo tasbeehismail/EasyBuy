524c9d518044c5ea8b6ff6480928cc0bcd21f9f1
tasbeehismail <tsbeehismaill@gmail.com>
2024-07-16 11:49:21 +0300

    Add cart model

---
+++ src/models/cart.js
@@ -0,0 +1,31 @@
+import {mongoose, Types} from 'mongoose';
+
+const cartProductSchema = new mongoose.Schema({
+  product: {
+    type: Types.ObjectId,
+    required: true,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  products: [cartProductSchema],    
+  total: {
+    type: Number,
+    required: true,
+    min: 0,
+  }
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+export default Cart;
