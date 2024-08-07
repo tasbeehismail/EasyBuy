ea555e2100ee966fcb0cc572d4ceb7361b4c96d2
tasbeehismail <tsbeehismaill@gmail.com>
2024-08-07 14:12:42 +0300

    Add review routes and controllers to handle CRUD operations

---
+++ src/controllers/review.js
@@ -0,0 +1,105 @@
+import AppError from '../utils/appError.js';
+import Review from '../models/review.js';
+import APIFeatures from '../utils/APIFeatures.js';
+
+export const getReviews = async (req, res, next) => {
+    const searchFields = ['user']; 
+
+    const { productId } = req.params;
+    const filter = productId ? { product: productId } : {};
+
+    const features = new APIFeatures(Review.find(filter), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .search(searchFields)
+        .paginate();
+
+    const reviewsList = await features.query;
+
+    if (!reviewsList || reviewsList.length === 0) {
+        return next(new AppError('Reviews not found', 404));
+    }
+
+    const totalReviews = await Review.countDocuments(filter);
+    const totalPages = Math.ceil(totalReviews / (parseInt(req.query.limit) || 10));
+    const page = parseInt(req.query.page) || 1;
+    const hasNextPage = page < totalPages;
+    const hasPreviousPage = page > 1;
+    const nextPage = hasNextPage ? page + 1 : null;
+    const previousPage = hasPreviousPage ? page - 1 : null;
+    const numOfReviews = reviewsList.length;
+    const result = {
+        totalReviews,
+        metaData: {
+            page,
+            limit: parseInt(req.query.limit) || 10,
+            totalPages,
+            hasNextPage,
+            hasPreviousPage,
+            nextPage,
+            previousPage
+        },
+        numOfReviews,
+        reviews: reviewsList
+    };
+    res.status(200).json({ message: 'Reviews fetched successfully', data: result });
+}
+
+
+export const getReview = async (req, res, next) => {
+    const review = await Review.findById({ _id: req.params.id })
+    .select('-__v -createdAt -updatedAt -createdBy -updatedBy');
+    if(!review){
+        return next(new AppError('Review not found', 404));
+    }
+    res.status(200).json({ data: review });
+}
+
+export const addReview = async (req, res, next) => {
+    const user_id = req.user._id;
+    const { comment, rating } = req.body;
+    const { productId } = req.params;
+
+    const review = await Review.create({ 
+        comment,
+        product: productId,
+        rating,
+        user: user_id,
+        createdBy: user_id,
+        updatedBy: user_id
+    });
+    res.status(200).json({ message: 'Review added successfully', data: review });
+}
+
+export const updateReview = async (req, res, next) => {
+    const user_id = req.user._id;
+    const { comment, rating } = req.body;
+    
+    const review = await Review.findById({ _id: req.params.id });
+
+    if (!review) {
+        return next(new AppError('Review not found', 404));
+    }
+
+    review.comment = comment || review.comment;
+    review.rating = rating || review.rating;
+    review.updatedBy = user_id;
+    await review.save();
+
+    res.status(200).json({ message: 'Review updated successfully', data: review });
+}
+
+
+export const deleteReview = async (req, res, next) => {
+    const id = req.params.id;
+    const review = await Review.findById(id);
+
+    if (!review) {
+        return next(new AppError('Review not found', 404));
+    }
+
+    await Review .findByIdAndDelete(id);
+
+    res.status(200).json({ message: 'Review deleted successfully' });
+}
---
+++ src/routes/review.routes.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import * as reviewController from '../controllers/review.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import * as schema from "../validation/review.js";
+import { validate } from "../services/validator.service.js";
+import { verifyToken } from "../services/auth.service.js";
+import { authorizeRoles } from "../middleware/authorizeRoles.js";
+import { isValidId } from "../validation/idValidation.js";
+
+const router = Router({ mergeParams: true }); 
+
+router.post('/:productId', 
+    verifyToken(),
+    authorizeRoles('user'),
+    validate(schema.addReview),
+    asyncHandler(reviewController.addReview)
+)
+
+router.get('/', 
+    isValidId(),
+    asyncHandler(reviewController.getReviews)
+)
+
+router.get('/:id',
+    isValidId(),
+    asyncHandler(reviewController.getReview)
+)
+
+router.patch('/:id',
+    verifyToken(),
+    authorizeRoles('user'),
+    validate(schema.updateReview),
+    asyncHandler(reviewController.updateReview)
+)
+
+router.delete('/:id',
+    verifyToken(),
+    authorizeRoles('user', 'admin'),
+    isValidId(),
+    asyncHandler(reviewController.deleteReview)
+)
+
+export default router;
